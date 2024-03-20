db8d17edb84cedb47f759dbfaadd9e76edd03504
Kazuya INOUE <bauer24tbp@gmail.com>
2024-03-20 12:03:34 +0900

    FactoryBotのモジュール名を省略するための記述を追加

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -62,4 +62,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  # モジュール名 (FactoryBot)を省略するために記述
+  config.include FactoryBot::Syntax::Methods
 end
