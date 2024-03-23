b3895648b41d1dec44cca11df6e218d7e3701331
Kazuya INOUE <bauer24tbp@gmail.com>
2024-03-23 22:06:44 +0900

    rubocop指摘のためfrozen_string_literal: trueを追加

--- app/controllers/api/v1/todos_controller.rb
+++ app/controllers/api/v1/todos_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Api::V1::TodosController < ApplicationController
   def index
     @todos = Todo.all.order(:id)
--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Todo < ApplicationRecord
   validates :title,      presence: true
   validates :done,       inclusion: { in: [true, false] }
