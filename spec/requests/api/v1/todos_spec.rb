20282643e34bb1de9d9eb7690339894c8c408c7d
Kazuya INOUE <bauer24tbp@gmail.com>
2024-03-23 22:07:14 +0900

    テストコードの修正

--- spec/requests/api/v1/todos_spec.rb
+++ spec/requests/api/v1/todos_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe 'Api::V1::Todos', type: :request do
@@ -10,7 +12,6 @@ RSpec.describe 'Api::V1::Todos', type: :request do
     it 'returns a list of todos' do
       todo = Todo.create(title: 'Test Todo', done: false)
       get '/api/v1/todos'
-      expect(response).to have_http_status(:success)
       expect(response.body).to include(todo.title)
     end
 
@@ -18,9 +19,6 @@ RSpec.describe 'Api::V1::Todos', type: :request do
       todo1 = Todo.create(title: 'Test Todo 1', done: false)
       todo2 = Todo.create(title: 'Test Todo 2', done: false)
       get '/api/v1/todos'
-      expect(response).to have_http_status(:success)
-      expect(response.body).to include(todo1.title)
-      expect(response.body).to include(todo2.title)
       expect(response.body.index(todo1.title)).to be < response.body.index(todo2.title)
     end
 
