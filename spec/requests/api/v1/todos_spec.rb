2bf94d54f3308438a45e35bedbc4f23fefa2659c
Kazuya INOUE <bauer24tbp@gmail.com>
2024-03-28 20:50:24 +0900

    itの中身を日本語に変更

--- spec/requests/api/v1/todos_spec.rb
+++ spec/requests/api/v1/todos_spec.rb
@@ -6,93 +6,93 @@ RSpec.describe 'Api::V1::Todos', type: :request do
   let(:request_header) { { 'Content-Type' => 'application/json' } }
 
   describe 'GET /index' do
-    it 'returns http success' do
+    it 'HTTPステータスが成功になること' do
       get '/api/v1/todos'
       expect(response).to have_http_status(:success)
     end
 
-    it 'returns a list of todos' do
+    it 'Todoのリストを返すこと' do
       todo = Todo.create(title: 'Test Todo', done: false)
       get '/api/v1/todos'
       expect(response.body).to include(todo.title)
     end
-
-    it 'returns a list of todos in the correct order' do
+    
+    it 'Todoのリストを正しい順番で返すこと' do
       todo1 = Todo.create(title: 'Test Todo 1', done: false)
       todo2 = Todo.create(title: 'Test Todo 2', done: false)
       get '/api/v1/todos'
       expect(response.body.index(todo1.title)).to be < response.body.index(todo2.title)
     end
-
-    it 'conforms to request schema' do
+    
+    it 'リクエストスキーマに準拠していること' do
       get '/api/v1/todos'
       assert_request_schema_confirm
     end
-
-    it 'conforms to response schema with 200 response code' do
+    
+    it 'レスポンスコード200でレスポンススキーマに準拠していること' do
       get '/api/v1/todos'
       assert_response_schema_confirm(200)
     end
   end
 
   describe 'GET /show' do
-    it 'returns http success' do
+    it 'HTTPステータスが成功になること' do
       todo = Todo.create(title: 'Test Todo', done: false)
       get "/api/v1/todos/#{todo.id}"
       expect(response).to have_http_status(:success)
     end
 
-    it 'returns a todo' do
+    it 'Todoを返すこと' do
       todo = Todo.create(title: 'Test Todo', done: false)
       get "/api/v1/todos/#{todo.id}"
       expect(response.body).to include(todo.title)
     end
-
-    it 'returns a 404 for a missing todo' do
+    
+    it '存在しないTodoに対して404を返すこと' do
       get '/api/v1/todos/999'
       expect(response).to have_http_status(:not_found)
     end
-
-    it 'conforms to request schema' do
+    
+    it 'リクエストスキーマに準拠していること' do
       todo = Todo.create(title: 'Test Todo', done: false)
       get "/api/v1/todos/#{todo.id}"
       assert_request_schema_confirm
     end
-
-    it 'conforms to response schema with 200 response code' do
+    
+    it 'レスポンスコード200でレスポンススキーマに準拠していること' do
       todo = Todo.create(title: 'Test Todo', done: false)
       get "/api/v1/todos/#{todo.id}"
       assert_response_schema_confirm(200)
     end
-
-    it 'conforms to response schema with 404 response code' do
+    
+    it 'レスポンスコード404でレスポンススキーマに準拠していること' do
       get '/api/v1/todos/999'
       assert_response_schema_confirm(404)
     end
   end
 
   describe 'POST /create' do
-    it 'returns http success' do
+    it 'HTTPステータスが成功になること' do
       post '/api/v1/todos', params: { todo: { title: 'Test Todo' } }.to_json, headers: request_header
       expect(response).to have_http_status(:success)
     end
 
-    it 'creates a new todo' do
+    it '新しいTodoを作成すること' do
       post '/api/v1/todos', params: { todo: { title: 'Test Todo' } }.to_json, headers: request_header
       expect(Todo.last.title).to eq('Test Todo')
     end
-
-    it 'conforms to request schema' do
+    
+    it 'リクエストスキーマに準拠していること' do
       post '/api/v1/todos', params: { todo: { title: 'Test Todo' } }.to_json, headers: request_header
       assert_request_schema_confirm
     end
-
-    it 'conforms to response schema with 200 response code' do
+    
+    it 'レスポンスコード200でレスポンススキーマに準拠していること' do
       post '/api/v1/todos', params: { todo: { title: 'Test Todo' } }.to_json, headers: request_header
       assert_response_schema_confirm(200)
     end
-
-    it 'conforms to response schema with 422 response code' do
+    
+    it 'レスポンスコード422でレスポンススキーマに準拠していること' do
       post '/api/v1/todos', params: { todo: { title: '' } }.to_json, headers: request_header
       assert_response_schema_confirm(422)
     end
