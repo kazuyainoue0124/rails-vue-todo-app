8a5b87a9721886a12d9a83c1dd10eec4e9aabf70
Kazuya INOUE <bauer24tbp@gmail.com>
2024-04-07 20:44:47 +0900

    storybookとvuetifyの連携

---
+++ typescript/vue-app/.storybook/StoryWrapper.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app>
+    <v-main>
+      <slot name="story" />
+    </v-main>
+  </v-app>
+</template>
+
+<style>
+.v-application__wrap {
+  min-height: auto !important;
+  /* これをしないと大きな余白が作られる */
+  /* !importantしないと設定が上書きされるもよう */
+}
+</style>
--- typescript/vue-app/.storybook/preview.ts
+++ typescript/vue-app/.storybook/preview.ts
@@ -1,7 +1,15 @@
 import type { Preview } from '@storybook/vue3'
+import { setup } from '@storybook/vue3';
+import { withVuetifyTheme } from './withVuetifyTheme.decorator';
+import vuetify from '@/plugins/vuetify'
+
+setup((app) => {
+  app.use(vuetify)
+});
 
 const preview: Preview = {
   parameters: {
+    decorators: { withVuetifyTheme },
     controls: {
       matchers: {
         color: /(background|color)$/i,
