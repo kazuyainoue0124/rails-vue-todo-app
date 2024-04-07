5f5ad2c1b16fd68017acd03201a104e8f5fd6612
Kazuya INOUE <bauer24tbp@gmail.com>
2024-04-07 20:43:05 +0900

    トップページをダミーデータに変更

--- typescript/vue-app/src/views/HomeView.vue
+++ typescript/vue-app/src/views/HomeView.vue
@@ -1,9 +1,11 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <v-container>
+    <p>Hoge 1</p>
+    <p>Hoge 2</p>
+    <p>Hoge 3</p>
+    <p>Hoge 4</p>
+    <p>Hoge 5</p>
+  </v-container>
 </template>
