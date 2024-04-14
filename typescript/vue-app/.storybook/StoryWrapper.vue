57abf14a6b51f7769c2da05ffb0a2d55960b2d06
Kazuya INOUE <bauer24tbp@gmail.com>
2024-04-14 20:46:47 +0900

    不要なstyleを削除

--- typescript/vue-app/.storybook/StoryWrapper.vue
+++ typescript/vue-app/.storybook/StoryWrapper.vue
@@ -5,11 +5,3 @@
     </v-main>
   </v-app>
 </template>
-
-<style>
-.v-application__wrap {
-  min-height: auto !important;
-  /* これをしないと大きな余白が作られる */
-  /* !importantしないと設定が上書きされるもよう */
-}
-</style>
