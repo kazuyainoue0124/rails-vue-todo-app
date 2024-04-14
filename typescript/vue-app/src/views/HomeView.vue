aa7c3884fe65fbb69be06a2eb4c28157e5145223
Kazuya INOUE <bauer24tbp@gmail.com>
2024-04-14 15:19:29 +0900

    HomeViewの実装

--- typescript/vue-app/src/views/HomeView.vue
+++ typescript/vue-app/src/views/HomeView.vue
@@ -1,11 +1,45 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const todos = ref([
+  { id: 1, title: 'TODO 1', description: 'TODO 1 の詳細' },
+  { id: 2, title: 'TODO 2', description: 'TODO 2 の詳細' },
+  { id: 3, title: 'TODO 3', description: 'TODO 3 の詳細' },
+]);
+
+const handleDelete = (id: number, title: string) => {
+  console.log(`${title} を削除しました。`);
+};
+</script>
 
 <template>
-  <v-container>
-    <p>Hoge 1</p>
-    <p>Hoge 2</p>
-    <p>Hoge 3</p>
-    <p>Hoge 4</p>
-    <p>Hoge 5</p>
+  <v-container class="mt-12">
+    <v-row justify="center">
+      <v-col cols="12" sm="8" md="6">
+        <h1 class="text-center">Todo List</h1>
+        <v-card class="pa-10">
+          <v-list>
+            <v-list-item
+              class="mb-6"
+              border
+              height="60"
+              lines="two"
+              rounded
+              v-for="todo in todos"
+              :key="todo.id"
+            >
+              <v-list-item-title>
+                {{ todo.title }}
+              </v-list-item-title>
+              <template v-slot:append>
+                <v-list-item-action>
+                  <v-icon @click="handleDelete(todo.id, todo.title)">mdi-delete</v-icon>
+                </v-list-item-action>
+              </template>
+            </v-list-item>
+          </v-list>
+        </v-card>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
