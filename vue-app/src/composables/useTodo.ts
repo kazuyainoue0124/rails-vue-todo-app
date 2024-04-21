ceeab4e1dba3ae9ca41a2c6d0872f7fee24cef46
Kazuya INOUE <bauer24tbp@gmail.com>
2024-04-21 14:48:35 +0900

    URLの指定を環境変数に変更

--- vue-app/src/composables/useTodo.ts
+++ vue-app/src/composables/useTodo.ts
@@ -1,9 +1,13 @@
 import axios from 'axios';
 
+
 const useTodo = () => {
+  // Vite の環境変数を読み込む
+  const API_URL = import.meta.env.VITE_API_URL;
+
   const addTodo = async (title: string, description: string) => {
     try {
-      await axios.post('http://localhost:3000/api/v1/todos', { title, description });
+      await axios.post(`${API_URL}/api/v1/todos`, { title, description });
       return { success: true, message: 'TODOを追加しました' };
     } catch (error) {
       return { success: false, message: 'TODOの追加に失敗しました' };
