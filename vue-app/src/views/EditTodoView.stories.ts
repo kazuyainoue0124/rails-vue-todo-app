057dda9ecb927752987e7a703054fbe4c798c466
Kazuya INOUE <bauer24tbp@gmail.com>
2024-04-23 20:29:26 +0900

    vue-tsc対応

--- vue-app/src/views/EditTodoView.stories.ts
+++ vue-app/src/views/EditTodoView.stories.ts
@@ -24,8 +24,5 @@ export const Primary: Story = {
   }),
   args: {
     //👇 The args you need here will depend on your component
-    id: 1,
-    title: 'DB設計に関する技術書を1冊購入する',
-    description: '『SQLアンチパターン』を買う'
   }
 };
