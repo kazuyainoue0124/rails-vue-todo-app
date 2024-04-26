18f9074ee4da9a3b04131ba48ffd78ddd9a05eff
Kazuya INOUE <bauer24tbp@gmail.com>
2024-04-26 20:23:57 +0900

    Storybook上でもPiniaを使えるよう修正

--- vue-app/.storybook/preview.ts
+++ vue-app/.storybook/preview.ts
@@ -2,6 +2,12 @@ import type { Preview } from '@storybook/vue3'
 import { setup } from '@storybook/vue3';
 import { withVuetifyTheme } from './withVuetifyTheme.decorator';
 import vuetify from '@/plugins/vuetify'
+import { createPinia } from 'pinia';
+
+const pinia = createPinia();
+setup((app) => {
+  app.use(pinia)
+});
 
 setup((app) => {
   app.use(vuetify)
